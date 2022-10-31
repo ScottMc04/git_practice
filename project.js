8629d40a67eef4e2f76371abb57dfbcb6622818d
Scott <scott.mclaughlin01@gmail.com>
2022-10-31 12:02:00 +0000

    First Commit

---
+++ project.js
@@ -0,0 +1,5 @@
+const list = ["'No snowflake in an avalanche ever feels responsible.'", "'You are not illiterate.'", "'If a turtle doesn’t have a shell, is it naked or homeless?'", "'You are the crispy noodle in the vegetarian salad of life.'",
+ "'Two days from now, tomorrow will be yesterday'"];
+
+ let num = Math.floor(Math.random() * 5);
+ console.log(list[num]);

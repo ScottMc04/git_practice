18bd18fe42888d28bd007bb6b01a2c8796227ee7
Scott <scott.mclaughlin01@gmail.com>
2022-10-31 12:16:30 +0000

    Second commit

--- project.js
+++ project.js
@@ -1,5 +1,7 @@
-const list = ["'No snowflake in an avalanche ever feels responsible.'", "'You are not illiterate.'", "'If a turtle doesn’t have a shell, is it naked or homeless?'", "'You are the crispy noodle in the vegetarian salad of life.'",
- "'Two days from now, tomorrow will be yesterday'"];
-
- let num = Math.floor(Math.random() * 5);
- console.log(list[num]);
+const list1 = ["He", "Your Mum", "Your sister", "Your Aunty", "She"];
+const list2 = ["is", "is not", "might possibly be", "is definatelty not"];
+const list3 = ["happy.", "weird.", "ugly.", "strange.", "beautiful."];
+let num = Math.floor(Math.random() * 5);
+let num2 = Math.floor(Math.random() * 4);
+let num3 = Math.floor(Math.random() * 5);
+console.log(list1[num], list2[num2], list3[num3]);
